034117da8dcdddac5b42053a1c24b01037ada3ff
freddiejbawden <freddiejbawden@gmail.com>
2017-11-14 14:56:06 +0000

    completed A1 and A2 - needs testing

--- statements.py
+++ statements.py
@@ -16,17 +16,72 @@ def add(lst,item):
 
 class Lexicon:
     """stores known word stems of various part-of-speech categories"""
-    # add code here
+    def __init__ (self):
+        self.stems = {}
+        self.validTags = ["P","N","A","I","T"]
+
+    def add (self, stem, cat):
+        if not(cat in self.validTags):
+            raise ValueError("Category: " + cat + " is not valid!")
+        for key in self.stems:
+            if (key == stem):
+                if not(cat in self.stems[key]):
+                    self.stems[stem].append(cat)
+                    return 0
+
+        self.stems[stem] = [cat]
+        return 1
+
+    def getAll(self, cat):
+        toReturn = []
+        for key in self.stems:
+            if cat in self.stems[key]:
+                toReturn.append(key)
+
+        return toReturn
+
+    def printStems (self):
+        print(self.stems)
 
 class FactBase:
     """stores unary and binary relational facts"""
-    # add code here
+    def __init__ (self):
+        self.unary = {}
+        self.binary = {}
+    def addUnary(self,pred,e1):
+        if pred in self.unary:
+            self.unary[pred].append(e1)
+        else:
+            self.unary[pred] = [e1]
+    def addBinary(self,pred,e1,e2):
+        if pred in self.binary:
+            self.binary[pred].append([e1,e2])
+        else:
+            self.binary[pred] = [[e1,e2]]
+
+    def queryUnary(self,pred,e1):
+        for key in self.unary:
+            if (key == pred):
+                if e1 in self.unary[key]:
+                    return True
+        return False
+
+    def queryBinary(self,pred,e1,e2):
+        for key in self.binary:
+            if (key == pred):
+                for exp in self.binary[key]:
+                    if (e1 in exp) and (e2 in exp):
+                        return True
+        return False
+
 
 import re
-from nltk.corpus import brown 
+from nltk.corpus import brown
 def verb_stem(s):
     """extracts the stem from the 3sg form of a verb, or returns empty string"""
-    # add code here
+    #stem is have
+    #see notepad for flow chart
+
 
 def add_proper_name (w,lx):
     """adds a name to a lexicon, checking if first letter is uppercase"""
@@ -63,6 +118,16 @@ def process_statement (lx,wlist,fb):
                     lx.add (stem,'T')
                     fb.addBinary ('T_'+stem,wlist[0],wlist[2])
     return msg
-                        
-# End of PART A.
 
+lx = Lexicon()
+lx.add("John","N")
+lx.add("Shelia","N")
+lx.add("John","T")
+fb = FactBase()
+fb.addUnary("duck","John")
+fb.addBinary("love","John","Mary")
+fb.addBinary("love","John","Mildrid")
+print(fb.queryUnary("duck","John"))
+print(fb.queryBinary("love","Mildrid","John"))
+
+# End of PART A.
