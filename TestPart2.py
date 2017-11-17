c05f316fbbb5ff6f68cb531a092a12c77c9cfa3a
freddiejbawden <freddiejbawden@gmail.com>
2017-11-17 19:23:49 +0000

    Changed TestPart2

--- TestPart2.py
+++ TestPart2.py
@@ -12,34 +12,39 @@ lx.add("likes","T")
 
 class TestPartB1(unittest.TestCase):
     def test_1(self):
-        self.assertEqual(noun_stem("country"),"countries")
+        self.assertEqual(noun_stem("countries"),"country")
     def test_2(self):
-        self.assertEqual(noun_stem("dog"),"dogs")
+        self.assertEqual(noun_stem("dogs"),"dog")
     def test_3(self):
-        self.assertEqual(noun_stem("ash"),"ashes")
+        self.assertEqual(noun_stem("ashes"),"ash")
     def test_4(self):
-        self.assertEqual(noun_stem("woman"),"women")
+        self.assertEqual(noun_stem("women"),"woman")
     def test_5(self):
         self.assertEqual(noun_stem("sheep"),"sheep")
     def test_6(self):
         self.assertEqual(noun_stem("buffalo"),"buffalo")
     def test_7(self):
-        self.assertEqual(noun_stem("man"),"men")
-
+        self.assertEqual(noun_stem("men"),"man")
     def test_8(self):
-        self.assertEqual(tag_word(lx,"John"),["P"])
+        self.assertEquals(noun_stem("butterflys"),"")
+
     def test_9(self):
-        self.assertEqual(sorted(tag_word(lx,"orange")),["A","Np"])
+        self.assertEqual(tag_word(lx,"John"),["P"])
     def test_10(self):
-        self.assertEqual(sorted(tag_word(lx,"fish")),sorted(["Ns","Np","Ip","Tp"]))
+        self.assertEqual(sorted(tag_word(lx,"orange")),["A","Np"])
     def test_11(self):
-        self.assertEqual(tag_word(lx,"a"),["AR"])
+        self.assertEqual(sorted(tag_word(lx,"fish")),sorted(["Ns","Np","Ip","Tp"]))
     def test_12(self):
-        self.assertEqual(tag_word(lx,"zxghqw"),[])
+        self.assertEqual(tag_word(lx,"a"),["AR"])
     def test_13(self):
+        self.assertEqual(tag_word(lx,"zxghqw"),[])
+    def test_14(self):
         x = "John likes fish"
         expout =[['P', 'Ts', 'Ns'], ['P', 'Ts', 'Np'], ['P', 'Ts', 'Tp'], ['P', 'Ts', 'Ip']]
         self.assertEqual(tag_words(lx,x.split(" ")),expout)
+    def test_15(self):
+        self.assertTrue("sheep" in unchanging_plurals_list)
 
 if __name__ == "__main__":
     unittest.main()
+
